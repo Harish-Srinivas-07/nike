d123383c8932ba6607d5477948410c6bb7f02e54
Harish-Srinivas-07 <sr.harishsrinivas@gmail.com>
2024-02-23 12:59:32 +0530

    Fresh Upload

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import Footer from './Footer';
+import Services from './Services';
+import CustomerReview from './CustomerReview';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+import PopularProducts from './PopularProducts';
+
+export {
+    Hero,
+    Footer,
+    Services,
+    CustomerReview,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    PopularProducts,
+};
